6acce45002f54c4875bcbe79e917a27b0ce21fd9
johnmuthua <caveomuthama@gmail.com>
2025-07-15 20:32:21 +0300

    feat: add to viewsets and urls

--- books/urls.py
+++ books/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import BookViewSet, OrderViewSet, CustomerViewSet, ReviewViewSet
+from .views import BookViewSet, OrderViewSet, CustomerViewSet, ReviewViewSet, AuthorViewSet, GenreViewSet, PublisherViewSet
 
 
 router = DefaultRouter()
@@ -8,6 +8,9 @@ router.register(r'books', BookViewSet)
 router.register(r'orders', OrderViewSet)
 router.register(r'customers', CustomerViewSet)
 router.register(r'reviews', ReviewViewSet)
+router.register(r'authors', AuthorViewSet)
+router.register(r'genres', GenreViewSet)
+router.register(r'publishers', PublisherViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),
--- books/views.py
+++ books/views.py
@@ -1,12 +1,12 @@
 from rest_framework import viewsets
-from .models.book import Book
+from .models.book import Book, Author, Genre, Publisher
 from .models.customer import Customer
 from .models.order import Order
 from .models.reviews import Review
 from .serializers.reviewserializer import ReviewSerializer
 from .serializers.orderserializer import OrderSerializer
 from .serializers.customerserializer import CustomerSerializer
-from .serializers.bookserializer import BookSerializer
+from .serializers.bookserializer import BookSerializer,AuthorSerializer,GenreSerializer,PublisherSerializer
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
@@ -23,3 +23,15 @@ class OrderViewSet(viewsets.ModelViewSet):
 class ReviewViewSet(viewsets.ModelViewSet):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+
+class AuthorViewSet(viewsets.ModelViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+class GenreViewSet(viewsets.ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+class PublisherViewSet(viewsets.ModelViewSet):
+    queryset = Publisher.objects.all()
+    serializer_class = PublisherSerializer
