dcf9b495c326884c26c3cc0355222b9a00a4c702
johnmuthua <caveomuthama@gmail.com>
2025-07-15 19:26:56 +0300

    feat: add serializers

--- books/models/reviews.py
+++ books/models/reviews.py
@@ -2,10 +2,10 @@ from django.db import models
 
 class Review(models.Model):
     book = models.ForeignKey('books.Book', on_delete=models.CASCADE, related_name='reviews')
-    customer = models.ForeignKey('books.Customer', on_delete=models.CASCADE, related_name='reviews')
+    reviewer = models.ForeignKey('books.Customer', on_delete=models.CASCADE, related_name='reviews')
     rating = models.PositiveIntegerField(default=1)
     comment = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Review for {self.book.title} by {self.customer.first_name} {self.customer.last_name} - Rating: {self.rating}"
+        return f"Review for {self.book.title} by {self.reviewer.first_name} {self.reviewer.last_name} - Rating: {self.rating}"
---
+++ books/serializers/orderserializer.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from models.order import Order
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = ['id', 'customer', 'book', 'quantity', 'order_date', 'status']
+        
